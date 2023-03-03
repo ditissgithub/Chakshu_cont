3b954ed31f40596e0b5a1af28ef6b8bcbc01a556
Satish gupta <95120727+ditissgithub@users.noreply.github.com>
2023-03-03 15:43:29 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+sed -i -e 's|bindIp: 127.0.0.1  # Enter 0.0.0.0,:: to bind to all IPv4 and IPv6 addresses or, alternatively, use the net.bindIpAll setting.|bindIp: 0.0.0.0  # Enter 0.0.0.0,:: to bind to all IPv4 and IPv6 addresses or, alternatively, use the net.bindIpAll setting.|g' /etc/mongod.conf 
+sed -i -e 's|#security|security|g' /etc/mongod.conf  
+sed -i "/security/a   \ \ authorization: 'enabled'" /etc/mongod.conf 
+sed -i -e 's|ExecStartPre=/usr/bin/chown mongod:mongod /var/run/mongodb|ExecStartPre=/usr/bin/chown -R root:root /var/run/mongodb|g' /usr/lib/systemd/system/mongod.service 
+sed -i -e 's|User=mongod|User=root|g' /usr/lib/systemd/system/mongod.service 
+sed -i -e 's|Group=mongod|Group=root|g' /usr/lib/systemd/system/mongod.service
+
+systemctl daemon-reload 
+systemctl restart mongod
+
 # install the chakshu repo on / directory of host machine because we mount the repo from / directory inside contianer
-RUN sed -i -e 's|mysqlclient==1.3.6|mysqlclient==2.1.1|g' /chakshu_conf/chakshu_backend-v2.5/requirements.txt
+sed -i -e 's|mysqlclient==1.3.6|mysqlclient==2.1.1|g' /chakshu_conf/chakshu_backend-v2.5/requirements.txt
 
 pip install -r /chakshu_conf/chakshu_backend-v2.5/requirements.txt
 
