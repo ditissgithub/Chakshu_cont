b3ec43a1d3d49b4d6913a17fc86df3d192aea085
Satish gupta <95120727+ditissgithub@users.noreply.github.com>
2023-03-03 14:31:19 +0530

    Update and rename pip_package_install.sh to entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# install the chakshu repo on / directory of host machine because we mount the repo from / directory inside contianer
+RUN sed -i -e 's|mysqlclient==1.3.6|mysqlclient==2.1.1|g' /chakshu_conf/chakshu_backend-v2.5/requirements.txt
+
+pip install -r /chakshu_conf/chakshu_backend-v2.5/requirements.txt
+
+# Start the MongoDB shell as a root user on the admin database
+mongo admin --eval "db.auth('root', 'root');"
+
+# Create the admin database
+mongo admin --eval "db.getSiblingDB('admin').createUser({user: 'admin', pwd: 'admin@@123', roles: [{ role: 'root', db: 'admin' }]})"
+
+cat > file1.js << EOF
+db.createUser({
+          user: 'root',
+          pwd: 'root',
+          roles: ['root']
+    });
+
+EOF
+
+
+
+
+
+mongo mongodb://admin:admin@@123@localhost/admin  file1.js
+
+mongo admin --eval "db.auth('root', 'root'); db.getSiblingDB('chakshudb').createUser({user: 'chakshu', pwd: 'chakshumanager', roles: [{ role: 'readWrite', db: 'chakshudb' }]})"
+
+
