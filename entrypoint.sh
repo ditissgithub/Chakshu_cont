edb1bc62d10d66c7ff31deab24b816c08dfd6d75
Satish gupta <95120727+ditissgithub@users.noreply.github.com>
2023-03-09 15:06:13 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,22 +12,34 @@ sed -i -e 's|mysqlclient==1.3.6|mysqlclient==2.1.1|g' /home/apps/chakshu/require
 
 pip install -r /home/apps/chakshu/requirements.txt
 
-# Start the MongoDB shell as a root user on the admin database
-mongo admin --eval "db.auth('root', 'root');"
+###create a root user on mongodb####
+# Set variables for the MongoDB server connection
+MONGO_HOST=localhost
+MONGO_PORT=27017
+MONGO_DB=admin
 
-# Create the admin database
-mongo admin --eval "db.getSiblingDB('admin').createUser({user: 'admin', pwd: 'admin123', roles: [{ role: 'root', db: 'admin' }]})"
-
-cat > file1.js << EOF
+# Connect to the MongoDB server and create the root user
+mongo --host $MONGO_HOST --port $MONGO_PORT <<EOF
+use $MONGO_DB
 db.createUser({
-          user: 'root',
-          pwd: 'root',
-          roles: ['root']
-    });
-
+  user: "root",
+  pwd: "root",
+  roles: [ { role: "root", db: "admin" } ]
+})
 EOF
 
-mongo mongodb://admin:admin123@localhost/admin  file1.js
+# Print the root user credentials
+echo "Root user created:"
+echo "Username: root"
+echo "Password: root"
+
+
+# Start the MongoDB shell as a root user on the admin database
+# Create the database as 'admin'
+mongo admin --eval "db.auth('root', 'root'); db.getSiblingDB('admin').createUser({user: 'admin', pwd: 'admin123', roles: [{ role: 'root', db: 'admin' }]})"
+
+#Create new database as 'chakshudb'
+
 
 mongo admin --eval "db.auth('root', 'root'); db.getSiblingDB('chakshudb').createUser({user: 'chakshu', pwd: 'chakshumanager', roles: [{ role: 'readWrite', db: 'chakshudb' }]})"
 
